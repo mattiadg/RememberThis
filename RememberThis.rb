6d708efd61d34a27888974fc93e7507394d122ee
mattiadg <mattiadigangi@gmail.com>
2013-10-07 22:14:19 +0200

    Aggiunto lo script

--- RememberThis.rb
+++ RememberThis.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/ruby
 if ARGV.size == 0 then 
-	print( "\nusage: " + __FILE__ + " 'text\n" ) 
+	print( "\nusage: " + __FILE__ + " 'text'\n" ) 
 else
     puts "\n" + ARGV[0].split.map { |s| s[0].chr.concat(/[[:punct:]]/ =~ s[-1].chr.to_s ? s[-1] : "") }.join(" ")
 end
